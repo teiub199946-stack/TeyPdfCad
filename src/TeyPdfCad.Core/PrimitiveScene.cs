ec899de169de75cc74e81aec2839ebc2831ccfd5
teiub199946-stack <teiub199946@gmail.com>
2026-09-17 18:05:04 +0300

    CORE-001 remove duplicate PrimitiveScene model

--- src/TeyPdfCad.Core/PrimitiveScene.cs
+++
@@ -1,9 +0,0 @@
-using TeyPdfCad.Core.Primitives;
-
-namespace TeyPdfCad.Core;
-
-public sealed class PrimitiveScene
-{
-    public List<LinePrimitive> Lines { get; } = new();
-    public List<TextPrimitive> Texts { get; } = new();
-}
