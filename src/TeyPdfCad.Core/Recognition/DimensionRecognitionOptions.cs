939adcac61f896f0cd71e5aea4a4f9cc23ba4008
teiub199946-stack <teiub199946@gmail.com>
2026-09-17 18:24:22 +0300

    CORE-001 add scene scale consensus options

--- src/TeyPdfCad.Core/Recognition/DimensionRecognitionOptions.cs
+++ src/TeyPdfCad.Core/Recognition/DimensionRecognitionOptions.cs
@@ -6,6 +6,8 @@ public sealed record DimensionRecognitionOptions
     public double MinConfidence { get; init; } = 0.75;
     public double MeasurementRelativeTolerance { get; init; } = 0.02;
     public double CanonicalScaleRelativeTolerance { get; init; } = 0.03;
+    public double ScaleConsensusRelativeTolerance { get; init; } = 0.02;
+    public int ScaleConsensusMinimumVotes { get; init; } = 2;
     public double PerpendicularAngleToleranceDegrees { get; init; } = 15.0;
     public double TextDistanceHeightMultiplier { get; init; } = 4.0;
     public double EndpointToleranceHeightMultiplier { get; init; } = 0.75;
