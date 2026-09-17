8b263a159b9eebbc5708690f3e5bbd5472773b2d
teiub199946-stack <teiub199946@gmail.com>
2026-09-17 19:33:28 +0300

    ADAPTER-001 add explicit AutoCAD plugin entry point

---
+++ src/TeyPdfCad.AutoCAD/PluginEntryPoint.cs
@@ -0,0 +1,21 @@
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+
+[assembly: ExtensionApplication(typeof(TeyPdfCad.AutoCAD.PluginEntryPoint))]
+[assembly: CommandClass(typeof(TeyPdfCad.AutoCAD.ReconstructionCommands))]
+
+namespace TeyPdfCad.AutoCAD;
+
+public sealed class PluginEntryPoint : IExtensionApplication
+{
+    public void Initialize()
+    {
+        var document = Application.DocumentManager.MdiActiveDocument;
+        document?.Editor.WriteMessage(
+            "\nTeyPdfCad loaded successfully. Commands: TEYPDFPING, TEYPDFANALYZE, TEYPDFRECONSTRUCT.\n");
+    }
+
+    public void Terminate()
+    {
+    }
+}
