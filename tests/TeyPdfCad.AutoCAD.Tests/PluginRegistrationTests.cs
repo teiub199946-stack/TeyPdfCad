5f22906562de06368086673a0059de946817c62d
teiub199946-stack <teiub199946@gmail.com>
2026-09-17 19:34:45 +0300

    ADAPTER-001 inspect registration metadata without AutoCAD runtime

--- tests/TeyPdfCad.AutoCAD.Tests/PluginRegistrationTests.cs
+++ tests/TeyPdfCad.AutoCAD.Tests/PluginRegistrationTests.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
 using Xunit;
 
 namespace TeyPdfCad.AutoCAD.Tests;
@@ -7,12 +9,34 @@ public sealed class PluginRegistrationTests
     [Fact]
     public void Assembly_declares_explicit_AutoCAD_plugin_entrypoint_and_command_class()
     {
-        var assembly = typeof(TeyPdfCad.AutoCAD.ReconstructionCommands).Assembly;
-        var attributeTypeNames = assembly.GetCustomAttributesData()
-            .Select(x => x.AttributeType.FullName)
+        var assemblyPath = typeof(TeyPdfCad.AutoCAD.ReconstructionCommands).Assembly.Location;
+        using var stream = File.OpenRead(assemblyPath);
+        using var peReader = new PEReader(stream);
+        var metadata = peReader.GetMetadataReader();
+
+        var attributeTypeNames = metadata.GetAssemblyDefinition()
+            .GetCustomAttributes()
+            .Select(handle => metadata.GetCustomAttribute(handle))
+            .Select(attribute => ResolveAttributeTypeName(metadata, attribute.Constructor))
+            .Where(name => name is not null)
             .ToArray();
 
         Assert.Contains("Autodesk.AutoCAD.Runtime.ExtensionApplicationAttribute", attributeTypeNames);
         Assert.Contains("Autodesk.AutoCAD.Runtime.CommandClassAttribute", attributeTypeNames);
+    }
+
+    private static string? ResolveAttributeTypeName(MetadataReader metadata, EntityHandle constructor)
+    {
+        if (constructor.Kind != HandleKind.MemberReference)
+            return null;
+
+        var memberReference = metadata.GetMemberReference((MemberReferenceHandle)constructor);
+        if (memberReference.Parent.Kind != HandleKind.TypeReference)
+            return null;
+
+        var typeReference = metadata.GetTypeReference((TypeReferenceHandle)memberReference.Parent);
+        var ns = metadata.GetString(typeReference.Namespace);
+        var name = metadata.GetString(typeReference.Name);
+        return string.IsNullOrEmpty(ns) ? name : $"{ns}.{name}";
     }
 }
