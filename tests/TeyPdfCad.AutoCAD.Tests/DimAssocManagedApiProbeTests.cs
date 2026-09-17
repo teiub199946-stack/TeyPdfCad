34aa13c6f83ceb153dab6914513144209538470f
teiub199946-stack <teiub199946@gmail.com>
2026-09-17 21:33:09 +0300

    SPIKE: verify managed dimension-association API surface by reflection

--- tests/TeyPdfCad.AutoCAD.Tests/DimAssocManagedApiProbeTests.cs
+++ tests/TeyPdfCad.AutoCAD.Tests/DimAssocManagedApiProbeTests.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Autodesk.AutoCAD.DatabaseServices;
 using Xunit;
 
@@ -6,15 +7,34 @@ namespace TeyPdfCad.AutoCAD.Tests;
 public sealed class DimAssocManagedApiProbeTests
 {
     [Fact]
-    public void Managed_api_exposes_dimension_association_types()
+    public void AutoCAD_2022_managed_api_does_not_export_native_dimassoc_types()
     {
-        var types = new[]
-        {
-            typeof(DimAssoc),
-            typeof(OsnapPointRef),
-            typeof(DimAssocPointType)
-        };
+        var exportedNames = typeof(Dimension).Assembly
+            .GetExportedTypes()
+            .Select(type => type.Name)
+            .ToArray();
 
-        Assert.All(types, type => Assert.NotNull(type));
+        Assert.DoesNotContain("DimAssoc", exportedNames);
+        Assert.DoesNotContain("OsnapPointRef", exportedNames);
+        Assert.DoesNotContain("DimAssocPointType", exportedNames);
+    }
+
+    [Fact]
+    public void Database_exposes_DIMASSOC_system_setting_but_dimension_has_no_public_assoc_member()
+    {
+        var dimAssocProperty = typeof(Database).GetProperty(
+            "DimAssoc",
+            BindingFlags.Public | BindingFlags.Instance);
+
+        Assert.NotNull(dimAssocProperty);
+
+        var publicDimensionAssocMembers = typeof(Dimension)
+            .GetMembers(BindingFlags.Public | BindingFlags.Instance)
+            .Where(member => member.Name.IndexOf("Assoc", StringComparison.OrdinalIgnoreCase) >= 0)
+            .Select(member => member.Name)
+            .Distinct(StringComparer.Ordinal)
+            .ToArray();
+
+        Assert.Empty(publicDimensionAssocMembers);
     }
 }
