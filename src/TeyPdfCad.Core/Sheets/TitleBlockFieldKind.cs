b1ac02bf9b5e843b33eba5794be2a966c14e9628
teiub199946-stack <teiub199946@gmail.com>
2026-09-18 23:46:32 +0300

    integration: preserve sheet semantic core

---
+++ src/TeyPdfCad.Core/Sheets/TitleBlockFieldKind.cs
@@ -0,0 +1,16 @@
+namespace TeyPdfCad.Core.Sheets;
+
+public enum TitleBlockFieldKind
+{
+    Unknown = 0,
+    DrawingName,
+    DrawingNumber,
+    Stage,
+    SheetNumber,
+    SheetCount,
+    Author,
+    Checker,
+    Approval,
+    Date,
+    Scale,
+}
