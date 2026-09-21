a5118a2a42daaa5f9f4da70d7152c95afc1d3bf9
teiub199946-stack <teiub199946@gmail.com>
2026-09-21 22:10:32 +0300

    feat: add deterministic DWG entity fingerprints

---
+++ src/TeyPdfCad.Dwg/DwgEntityFingerprint.cs
@@ -0,0 +1,128 @@
+using System.Globalization;
+using System.Text;
+using ACadSharp.Entities;
+using CSMath;
+
+namespace TeyPdfCad.Dwg;
+
+public static class DwgEntityFingerprint
+{
+    public static string ComputeGeometry(Entity entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+
+        return entity switch
+        {
+            Line line => Join("Line", Point(line.StartPoint), Point(line.EndPoint)),
+            DimensionLinear dimension => Join(
+                "DimensionLinear",
+                Point(dimension.FirstPoint),
+                Point(dimension.SecondPoint),
+                Point(dimension.DefinitionPoint),
+                Number(dimension.Rotation)),
+            DimensionAligned dimension => Join(
+                "DimensionAligned",
+                Point(dimension.FirstPoint),
+                Point(dimension.SecondPoint),
+                Point(dimension.DefinitionPoint)),
+            DimensionArc dimension => Join(
+                "DimensionArc",
+                Point(dimension.Center),
+                Point(dimension.FirstPoint),
+                Point(dimension.SecondPoint),
+                Point(dimension.DefinitionPoint),
+                Number(dimension.StartAngle),
+                Number(dimension.EndAngle)),
+            Dimension dimension => Join(
+                "Dimension",
+                Point(dimension.DefinitionPoint),
+                Number(dimension.Measurement)),
+            Leader leader => Join(
+                "Leader",
+                string.Join(";", leader.Vertices.Select(Point)),
+                Number(leader.TextHeight),
+                leader.ArrowHeadEnabled ? "1" : "0"),
+            TextEntity text => Join(
+                "Text",
+                Point(text.InsertPoint),
+                Number(text.Height),
+                Number(text.Rotation),
+                text.Value ?? string.Empty),
+            AttributeEntity attribute => Join(
+                "Attribute",
+                Point(attribute.InsertPoint),
+                Number(attribute.Height),
+                Number(attribute.Rotation),
+                attribute.Tag ?? string.Empty,
+                attribute.Value ?? string.Empty),
+            Insert insert => Join(
+                "Insert",
+                insert.Block?.Name ?? string.Empty,
+                Point(insert.InsertPoint),
+                Number(insert.XScale),
+                Number(insert.YScale),
+                Number(insert.ZScale),
+                Number(insert.Rotation)),
+            Hatch hatch => Join(
+                "Hatch",
+                hatch.IsSolid ? "1" : "0",
+                Number(hatch.PatternAngle),
+                Number(hatch.PatternScale),
+                ComputeHatchBoundary(hatch)),
+            LwPolyline polyline => Join(
+                "LwPolyline",
+                polyline.IsClosed ? "1" : "0",
+                string.Join(";", polyline.Vertices.Select(vertex =>
+                    Number(vertex.Location.X) + "," + Number(vertex.Location.Y)))),
+            _ => Join(entity.GetType().Name, entity.ObjectName ?? string.Empty)
+        };
+    }
+
+    public static string ComputeHatchBoundary(Hatch hatch)
+    {
+        ArgumentNullException.ThrowIfNull(hatch);
+
+        var exploded = hatch.Explode()
+            .Select(ComputeGeometry)
+            .OrderBy(value => value, StringComparer.Ordinal)
+            .ToArray();
+        return Join(
+            "HatchBoundary",
+            hatch.Paths.Count.ToString(CultureInfo.InvariantCulture),
+            string.Join(";", exploded));
+    }
+
+    public static string ComputeOutput(Entity entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+        var metadata = CandidateMetadataCodec.TryRead(entity, out var candidate)
+            ? Join(candidate.CandidateId, candidate.Role)
+            : string.Empty;
+        return Join(
+            entity.GetType().Name,
+            ComputeGeometry(entity),
+            entity.Layer?.Name ?? string.Empty,
+            metadata);
+    }
+
+    private static string Point(XYZ point)
+        => Number(point.X) + "," + Number(point.Y) + "," + Number(point.Z);
+
+    private static string Number(double value)
+        => value.ToString("R", CultureInfo.InvariantCulture);
+
+    private static string Join(params string[] values)
+    {
+        var builder = new StringBuilder();
+        foreach (var value in values)
+        {
+            builder
+                .Append(value.Length.ToString(CultureInfo.InvariantCulture))
+                .Append(':')
+                .Append(value)
+                .Append('|');
+        }
+
+        return builder.ToString();
+    }
+}
